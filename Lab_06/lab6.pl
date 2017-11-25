22d36f3b4f430a6d8d5afbee282c66316a25b715
DHANUSH V <dhanush167@gmail.com>
2017-11-25 09:26:19 +0530

    Update lab6.pl

--- Lab_06/lab6.pl
+++ Lab_06/lab6.pl
@@ -1,7 +1,6 @@
+#!F:/Xampp/perl/bin/perl
 #Write a Perl program to display a digital clock which displays the 
 #current time of the server.
-
-#!F:/Xampp/perl/bin/perl
 print "refresh:1 \n";
 print "content-type:text/html \n\n";
 
